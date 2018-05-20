1aa99b54fab98f851469529c93f9482e9f9bb07b
sechkomar <sechko.mar@yandex.ru>
2018-05-21 00:33:59 +0300

    add module for range slider

--- application/browse_window.py
+++ application/browse_window.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from PyQt5 import QtCore, QtWidgets
+from PyQt5 import QtCore, QtWidgets, QtGui
 from PyQt5.QtWidgets import QFileDialog
 
 
@@ -8,52 +8,52 @@ class Ui_browseWindow(object):
     def setupUi(self, browseWindow):
         browseWindow.setObjectName("browseWindow")
         browseWindow.resize(510, 425)
-        self.centralwidget = QtWidgets.QWidget(browseWindow)
-        self.centralwidget.setObjectName("centralwidget")
+        self.centralWidget = QtWidgets.QWidget(browseWindow)
+        self.centralWidget.setObjectName("centralwidget")
 
-        self.filePath = QtWidgets.QPlainTextEdit(self.centralwidget)
+        self.filePath = QtWidgets.QLineEdit(self.centralWidget)
         self.filePath.setGeometry(QtCore.QRect(20, 130, 331, 31))
         self.filePath.setObjectName("filePath")
 
-        self.appDescription = QtWidgets.QLabel(self.centralwidget)
-        self.appDescription.setGeometry(QtCore.QRect(30, 30, 421, 61))
-        self.appDescription.setObjectName("appDescription")
+        self.appDescriptionLabel = QtWidgets.QLabel(self.centralWidget)
+        self.appDescriptionLabel.setGeometry(QtCore.QRect(30, 30, 421, 61))
+        self.appDescriptionLabel.setObjectName("appDescription")
 
-        self.browseFileButton = QtWidgets.QPushButton(self.centralwidget)
+        self.browseFileButton = QtWidgets.QPushButton(self.centralWidget)
         self.browseFileButton.setGeometry(QtCore.QRect(360, 130, 31, 31))
         self.browseFileButton.setObjectName("browseFilePath")
 
-        self.filePathLabel = QtWidgets.QLabel(self.centralwidget)
+        self.filePathLabel = QtWidgets.QLabel(self.centralWidget)
         self.filePathLabel.setGeometry(QtCore.QRect(20, 100, 67, 17))
         self.filePathLabel.setObjectName("filePathLabel")
 
-        self.curveModeComboBox = QtWidgets.QComboBox(self.centralwidget)
+        self.curveModeComboBox = QtWidgets.QComboBox(self.centralWidget)
         self.curveModeComboBox.setGeometry(QtCore.QRect(20, 210, 281, 31))
         self.curveModeComboBox.setObjectName("curveModeComboBox")
         self.curveModeComboBox.addItem("")
         self.curveModeComboBox.addItem("")
         self.curveModeComboBox.addItem("")
 
-        self.curveModeLabel = QtWidgets.QLabel(self.centralwidget)
+        self.curveModeLabel = QtWidgets.QLabel(self.centralWidget)
         self.curveModeLabel.setGeometry(QtCore.QRect(20, 180, 141, 17))
         self.curveModeLabel.setObjectName("curveModeLabel")
 
-        self.colorsLabel = QtWidgets.QLabel(self.centralwidget)
+        self.colorsLabel = QtWidgets.QLabel(self.centralWidget)
         self.colorsLabel.setGeometry(QtCore.QRect(20, 270, 131, 17))
         self.colorsLabel.setObjectName("colorsLabel")
 
-        self.colorsComboBox = QtWidgets.QComboBox(self.centralwidget)
+        self.colorsComboBox = QtWidgets.QComboBox(self.centralWidget)
         self.colorsComboBox.setGeometry(QtCore.QRect(20, 300, 281, 31))
         self.colorsComboBox.setObjectName("colorsComboBox")
         self.colorsComboBox.addItem("")
         self.colorsComboBox.addItem("")
         self.colorsComboBox.addItem("")
 
-        self.okButton = QtWidgets.QPushButton(self.centralwidget)
+        self.okButton = QtWidgets.QPushButton(self.centralWidget)
         self.okButton.setGeometry(QtCore.QRect(390, 350, 101, 29))
         self.okButton.setObjectName("okButton")
 
-        browseWindow.setCentralWidget(self.centralwidget)
+        browseWindow.setCentralWidget(self.centralWidget)
 
         self.menubar = QtWidgets.QMenuBar(browseWindow)
         self.menubar.setGeometry(QtCore.QRect(0, 0, 508, 23))
@@ -96,12 +96,12 @@ class Ui_browseWindow(object):
 
     def selectFile(self):
         file_path = QFileDialog.getOpenFileName()[0]
-        self.filePath.setPlainText(file_path)
+        self.filePath.setText(file_path)
 
     def retranslateUi(self, browseWindow):
         _translate = QtCore.QCoreApplication.translate
         browseWindow.setWindowTitle(_translate("browseWindow", "Pixel plot creator"))
-        self.appDescription.setText(_translate("browseWindow",
+        self.appDescriptionLabel.setText(_translate("browseWindow",
                                                "<html><head/><body><p><span style=\" font-size:12pt;\">Please select "
                                                "the file in .csv format, choose a curve mode </span></p><p><span "
                                                "style=\" font-size:12pt;\">and colors to create a pixel plot. "
@@ -109,6 +109,8 @@ class Ui_browseWindow(object):
 
         self.browseFileButton.setText(_translate("browseWindow", "..."))
         self.filePathLabel.setText(_translate("browseWindow", "File path"))
+
+        self.filePath.setFont(QtGui.QFont("Noto Sans CJK TC Light", 10))
 
         self.curveModeComboBox.setCurrentText(_translate("browseWindow", "choose curve mode"))
         self.curveModeComboBox.setItemText(0, _translate("browseWindow", "Gorizontal mode"))
